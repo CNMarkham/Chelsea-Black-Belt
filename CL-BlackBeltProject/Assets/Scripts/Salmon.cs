4d219db0a27615e8697a906b00748653c71ab778
Sussy baka <132835392+CNMarkham@users.noreply.github.com>
2024-02-06 19:53:48 -0500

    Revert "working on making sushi roll cut into individual sushi rolls"
    This reverts commit 1df662b3919b4dd6e5c6f6e7d26b9b47603f836f.

--- CL-BlackBeltProject/Assets/Scripts/Salmon.cs
+++ CL-BlackBeltProject/Assets/Scripts/Salmon.cs
@@ -7,6 +7,7 @@ public class Salmon : MonoBehaviour, ISwipeable
     public GameObject salmonToEnable;
     public void GetSwiped()
     {
+        print("swiped 3");
         gameObject.SetActive(false);
         salmonToEnable.SetActive(true);
     }
