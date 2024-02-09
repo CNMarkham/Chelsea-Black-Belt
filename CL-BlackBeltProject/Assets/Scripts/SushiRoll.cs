49d6eb36f847608e6673f93a5b6ea33ade7ca669
Chelsea <markhamonca@codeninjas.com>
2024-02-08 19:59:30 -0500

    planned a bit on how the game will work

--- CL-BlackBeltProject/Assets/Scripts/SushiRoll.cs
+++ CL-BlackBeltProject/Assets/Scripts/SushiRoll.cs
@@ -7,16 +7,30 @@ public class SushiRoll : MonoBehaviour, ISwipeable
     public GameObject rice;
     public GameObject noriPrefab;
     public GameObject thinSalmonPrefab;
-    private bool alreadyCut;
+    private int cutCounter;
+    public GameObject sushiToEnable;
 
     public void GetSwiped()
     {
-        if (rice.activeSelf && noriPrefab.activeSelf && thinSalmonPrefab.activeSelf)
+        if (cutCounter == 0)
         {
+            if (rice.activeSelf && noriPrefab.activeSelf && thinSalmonPrefab.activeSelf)
+            {
             rice.SetActive(false);
             noriPrefab.SetActive(false);
             thinSalmonPrefab.SetActive(false);
             GetComponent<MeshRenderer>().enabled = true;
+            }
         }
+
+        if (cutCounter == 1)
+        {
+            gameObject.SetActive(false);
+            sushiToEnable.SetActive(true);
+
+        }
+        cutCounter += 1;
+
+
     }
 }
