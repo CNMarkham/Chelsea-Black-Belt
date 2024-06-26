dbedd87a0f92e29b2656d4861cbe769d9a954221
Chelsea <markhamonca@codeninjas.com>
2024-06-26 19:59:34 -0400

    fixed pop up thing (need to fix the camera afterwards)

--- CL-BlackBeltProject/Assets/Scripts/TutorialMenu.cs
+++ CL-BlackBeltProject/Assets/Scripts/TutorialMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class TutorialMenu : MonoBehaviour
 {
+    public GameObject popUpMenu;
+    public GameObject gameCursor;
+    public GameObject gameTimer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,9 +31,18 @@ public class TutorialMenu : MonoBehaviour
     public void ChangeLevel()
     {
         GameTime.levelCounter = 1;
-        SceneManager.LoadScene(0);
-    //the restart pop up will disappear
-    //make level counter 1 and next level will be played 
+        //Turn Off Pop up
+        popUpMenu.SetActive(false);
+        //Unlock Cursors
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        //See the cursor
+        gameCursor.SetActive(true);
+        //Make timer appear
+        gameTimer.SetActive(true);
+        //  SceneManager.LoadScene(0);
+        //the restart pop up will disappear
+        //make level counter 1 and next level will be played 
     }
 
 }
