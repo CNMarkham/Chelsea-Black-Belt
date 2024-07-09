2078a4281d03d4a7320490831d2411ae50644f03
Chelsea <markhamonca@codeninjas.com>
2024-07-09 19:55:24 -0400

    worked on credit scene again :(
    need to look for inspiration next time

--- CL-BlackBeltProject/Assets/Scripts/CreditsMover.cs
+++ CL-BlackBeltProject/Assets/Scripts/CreditsMover.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditsMover : MonoBehaviour
 {
@@ -8,12 +9,17 @@ public class CreditsMover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("DelayedSceneLoader", 43f);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    void DelayedSceneLoader()
+    {
+        SceneManager.LoadScene(1);
     }
 }
